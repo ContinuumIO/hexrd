3de7833a0fb93e0d0a5768f1f25eb2a0a2c9965e
Michael Grant <mgrant@continuum.io>
2015-12-09 13:33:27 -0600

    Attempt to unify CPU/GPU

--- hexrd/xrd/transforms_NUMBA.py
+++ hexrd/xrd/transforms_NUMBA.py
@@ -1,83 +1,104 @@
-from __future__ import division
-from math import hypot, fabs, cos, sin, acos, pi, atan2, asin, sqrt
-from numba import guvectorize, jit
+from __future__ import division, print_function
+from math import isnan, hypot, fabs, cos, sin, acos, pi, atan2, asin, sqrt
 import numpy as np
+import numba as nb
 
-#
-# We don't necessarily need to expose full Python versions
-# of all of these functions, but we do anyway for testing.
-#
+TARGET = 'cpu'
+if TARGET == 'cuda':
+    @nb.cuda.jit(device=True)
+    def hypot(x,y):
+        return sqrt(x**2+y**2)
+    def jit(signature):
+        return nb.cuda.jit(signature, device=True)
+    def guvectorize(sig1, sig2):
+        return nb.guvectorize(sig1, sig2, nopython=True, target='cuda')
 
-@jit('f8(f8[:])', nopython=True, nogil=True)
+elif TARGET == 'parallel':
+    def jit(signature):
+        return nb.jit(signature, nopython=True, nogil=True, target='parallel')
+    def guvectorize(sig1, sig2):
+        return nb.guvectorize(sig1, sig2, nopython=True, target='parallel')
+
+else:
+    def jit(signature):
+        return nb.jit(signature, nopython=True, nogil=True)
+    def guvectorize(sig1, sig2):
+        return nb.guvectorize(sig1, sig2, nopython=True)
+
+
+@jit('f8(f8[:])')
 def norm3_(vec):
     return hypot(hypot(vec[0], vec[1]), vec[2])
 
-@guvectorize('(f8[:],f8[:],f8[:])','(n),(n)->()', nopython=True)
-def norm3_gufunc_(vec, dummy, out):
+@guvectorize('(f8[:],f8[:])','(n)->()')
+def norm3_gufunc_(vec, out):
     out[0] = norm3_(vec)
 
-VEC3 = np.empty((3,))
 def norm3(vec, out=None):
     '''
     Optimized vector norm for 3 vectors (and arrays of same).
     '''
-    return norm3_gufunc_(vec, VEC3, out)
+    if vec.shape[-1] != 3:
+        raise RuntimeError('Array of 3-vectors expected')
+    return norm3_gufunc_(vec, out)
 
 
-@jit('f8(f8[:])', nopython=True, nogil=True)
-def normq_(vec):
-    return hypot(hypot(vec[0], vec[1]), hypot(vec[2], vec[3]))
+@jit('(f8[:],f8,f8[:])')
+def div3_(vec, scl, out):
+    out[0] = vec[0] / scl
+    out[1] = vec[1] / scl
+    out[2] = vec[2] / scl
 
-@guvectorize('(f8[:],f8[:],f8[:])','(n),(n)->()', nopython=True)
-def normq_gufunc_(vec, dummy, out):
-    out[0] = normq_(vec)
+@guvectorize('(f8[:],f8,f8[:])','(n),()->(n)')
+def div3_gufunc_(vec, scl, out):
+	div3_(vec, scl, out)
 
-VEC4 = np.empty((4,))
-def normq(vec, out=None):
+def div3(vec, scl, out=None):
     '''
-    Optimized norm for quaternion (and arrays of same).
+    Optimized division for 3 vectors (and arrays of same).
     '''
-    return normq_gufunc_(vec, VEC4, out)
+    if vec.shape[-1] != 3:
+        raise RuntimeError('Array of 3-vectors expected')
+    return div3_gufunc_(vec, scl, out)
+    
+
+@jit('f8(f8[:],f8[:])')
+def normalize3_(vec, out):
+    tmp = norm3_(vec)
+    div3_(vec, tmp, out)
+    return tmp
+
+@guvectorize('(f8[:],f8[:])','(n)->()')
+def normalize3_gufunc_(vec, out):
+    out[0] = normalize3_(vec, vec)
+
+def normalize3(vec, out=None):
+    '''
+    Normalizing 3 vectors (and arrays of same).
+    '''
+    if vec.shape[-1] != 3:
+        raise RuntimeError('Array of 3-vectors expected')
+    return normalize3_gufunc_(vec, out)
 
 
-@jit('f8(f8[:],f8[:])', nopython=True, nogil=True)
+@jit('f8(f8[:],f8[:])')
 def dot3_(a, b):
     return a[0] * b[0] + a[1] * b[1] + a[2] * b[2]
 
-@guvectorize('(f8[:],f8[:],f8[:],f8[:])', '(n),(n),(n)->()', nopython=True)
-def dot3_gufunc_(a, b, dummy, out):
+@guvectorize('(f8[:],f8[:],f8[:])', '(n),(n)->()')
+def dot3_gufunc_(a, b, out):
     out[0] = dot3_(a, b)
 
-VEC3 = np.empty((3,))
 def dot3(a, b, out=None):
     '''
     Optimized dot product for 3 vectors (and arrays of same).
     '''
-    return dot3_gufunc_(a, b, VEC3, out)
+    if a.shape[-1] != 3 or b.shape[-1] != 3:
+        raise RuntimeError('Inputs must be arrays of 3-vectors')
+    return dot3_gufunc_(a, b, out)
 
 
-tiny64 = np.finfo(np.float64).tiny
-@jit(nopython=True, nogil=True)
-def quatToHomochoric_(q, h):
-    phi = 2.0 * acos(q[0])
-    tmp = (0.75*(phi-sin(phi)))**(1.0/3.0) / (tiny64 + norm3_(q[1:]))
-    h[0] = q[1] * tmp
-    h[1] = q[2] * tmp
-    h[2] = q[3] * tmp
-    return h
-
-@guvectorize('(f8[:],f8[:],f8[:],f8[:])', '(m),(m),(n)->(n)', nopython=True)
-def quatToHomochoric_gufunc_(q, d4, d3, h):
-    quatToHomochoric_(q, h)
-
-def quatToHomochoric(quats, out=None):
-    '''
-    Converts quaternions (or arrays of same) into homochoric coordinates.
-    '''
-    return quatToHomochoric_gufunc_(quats, VEC4, VEC3, out)
-
-
-@jit(nopython=True, nogil=True)
+@jit('(f8[:],f8[:,:])')
 # Reference: http://fabiensanglard.net/doom3_documentation/37726-293748.pdf
 # I needed to swap elements and negate some signs to get the results to
 # match up with the original C code. I'm assuming this is due to differences
@@ -110,8 +131,8 @@ def quatToRotMat_(q, m):
     m[2,0] = wy2 - xz2
     m[0,2] = wy2 + xz2
 
-@guvectorize('(f8[:],f8[:],f8[:],f8[:,:])', '(m),(m),(n)->(n,n)', nopython=True)
-def quatToRotMat_gufunc_(q, d4, d3, m):
+@guvectorize('(f8[:],f8[:],f8[:,:])', '(m),(n)->(n,n)')
+def quatToRotMat_gufunc_(q, d3, m):
     quatToRotMat_(q, m)
 
 def quatToRotMat(quats, out=None):
@@ -119,74 +140,64 @@ def quatToRotMat(quats, out=None):
     Make rotation matrices from unit quaternions
 
     """
-    return quatToRotMat_gufunc_(quats, VEC4, VEC3, out)
-
+    if quats.shape[-1] != 4:
+        raise RuntimeError('Input must be an array of quaternions (4-vectors)')
+    return quatToRotMat_gufunc_(quats, np.empty((3,)), out)
 
 
 sqr05 = sqrt(0.5)
-@jit('(f8[:],f8[:,:])', nopython=True, nogil=True)
-def vecMVToSymm_(A,symm):
+@nb.jit(nopython=True)
+def vecMVToSymm(A):
+    symm = np.empty((3,3))
     symm[0, 0] = A[0]
     symm[0, 1] = symm[1, 0] = A[5] * sqr05
     symm[0, 2] = symm[2, 0] = A[4] * sqr05
     symm[1, 1] = A[1]
     symm[1, 2] = symm[2, 1] = A[3] * sqr05
     symm[2, 2] = A[2]
-
-@guvectorize('(f8[:],f8[:],f8[:],f8[:,:])','(n6),(n6),(n3)->(n3,n3)', nopython=True)
-def vecMVToSymm_gufunc_(A, d6, d3, symm):
-    vecMVToSymm_(A, symm)
-
-VEC6 = np.empty((6,))
-def vecMVToSymm(A, out=None):
-    """
-    convert from Mandel-Voigt vector to symmetric matrix representation
-    """
-    return vecMVToSymm_gufunc_(A, VEC6, VEC3, out)
+    return symm
 
 
 etaFrameTol = 1.0 - sqrt(np.finfo(np.float32).eps)
-@jit('(f8[:],f8[:],f8[:,:])', nopython=True, nogil=True)
-def etaFrameToRotMat_(beamVec, etaVec, rMat):
-    np.divide(beamVec, -norm3_(beamVec), rMat[:, 2])
-    dt = dot3_(rMat[:, 2], etaVec)
-    if fabs(dt) >= norm3_(etaVec) * etaFrameTol:
+@nb.jit(nopython=True)
+def etaFrameToRotMat(beamVec, etaVec):
+    rMat = np.empty((3,3))
+    tmp = hypot(hypot(beamVec[0], beamVec[1]), beamVec[2])
+    rMat[:, 2] = beamVec / -tmp
+    dt = rMat[0, 2] * etaVec[0] + rMat[1, 2] * etaVec[1] + rMat[2, 2] * etaVec[2]
+    tmp = hypot(hypot(etaVec[0], etaVec[1]), etaVec[2])
+    if fabs(dt) >= tmp * etaFrameTol:
         rMat[0, 0] = rMat[1, 0] = rMat[2, 0] = 0.0
         rMat[0, 1] = rMat[1, 1] = rMat[2, 1] = np.nan
     else:
         rMat[0, 0] = etaVec[0] - dt * rMat[0, 2]
         rMat[1, 0] = etaVec[1] - dt * rMat[1, 2]
         rMat[2, 0] = etaVec[2] - dt * rMat[2, 2]
-        np.divide(rMat[:, 0], norm3_(rMat[:, 0]), rMat[:, 0])
+        tmp = hypot(hypot(rMat[0, 0], rMat[1, 0]), rMat[2, 0])
+        rMat[:, 0] /= tmp
         rMat[0, 1] = rMat[1, 2] * rMat[2, 0] - rMat[2, 2] * rMat[1, 0]
         rMat[1, 1] = rMat[2, 2] * rMat[0, 0] - rMat[0, 2] * rMat[2, 0]
         rMat[2, 1] = rMat[0, 2] * rMat[1, 0] - rMat[1, 2] * rMat[0, 0]
-
-@guvectorize('(f8[:],f8[:],f8[:],f8[:,:])','(n3),(n3),(n3)->(n3,n3)', nopython=True)
-def etaFrameToRotMat_gufunc_(beamVec, etaVec, dummy, rMat):
-    etaFrameToRotMat_(beamVec, etaVec, rMat)
-
-def etaFrameToRotMat(beamVec, etaVec, out=None):
-    return etaFrameToRotMat_gufunc_(beamVec, etaVec, VEC3, out)
+    return rMat
 
 
-@jit('f8[:](f8[:,:],f8[:])', nopython=True, nogil=True)
-def mv3_(a, b):
-    c = np.empty((3,))
+@jit('(f8[:,:],f8[:],f8[:])')
+def mv3_(a, b, c):
     c[0] = dot3_(a[0], b)
     c[1] = dot3_(a[1], b)
     c[2] = dot3_(a[2], b)
-    return c
 
-@jit('(f8[:],f8,f8,f8[:,:],f8[:,:],f8,f8[:,:],f8[:,:],f8[:,:])', nopython=True, nogil=True)
+@jit('(f8[:],f8,f8,f8[:,:],f8[:,:],f8,f8[:,:],f8[:,:],f8[:,:])')
 def oscillAnglesOfHKL_(hkl, cc, sc, rMat_c, bMat, wavelength, vMat_s, rMat_e, oangs):
     # reciprocal lattice vector in SAMPLE frame
-    gHat_s = mv3_(vMat_s, mv3_(rMat_c, mv3_(bMat, hkl)))
-    
-    # Normalize
-    nrm0 = norm3_(gHat_s)
-    np.divide(gHat_s, nrm0, gHat_s)
+    tmp = np.empty((3,))
+    gHat_s = np.empty((3,))
+    mv3_(bMat, hkl, gHat_s)
+    mv3_(rMat_c, gHat_s, tmp)
+    mv3_(vMat_s, tmp, gHat_s)
 
+    # Normalize
+    nrm0 = normalize3_(gHat_s, gHat_s)
     # sin of the Bragg angle assoc. with wavelength
     sintht = 0.5 * wavelength * nrm0              
 
@@ -215,7 +226,6 @@ def oscillAnglesOfHKL_(hkl, cc, sc, rMat_c, bMat, wavelength, vMat_s, rMat_e, oa
     oangs[0,2] = rhsAng - phaseAng
     oangs[1,2] = pi - rhsAng - phaseAng
 
-    tmp = np.empty((3,))
     oangs[0,0] = oangs[1,0] = 2.0 * asin(sintht)
     for i in range(2):
         co = cos(oangs[i, 2])
@@ -226,10 +236,10 @@ def oscillAnglesOfHKL_(hkl, cc, sc, rMat_c, bMat, wavelength, vMat_s, rMat_e, oa
         oangs[i,1] = atan2(dot3_(tmp, rMat_e[:,1]), dot3_(tmp, rMat_e[:,0]))
 
 @guvectorize(
-    '(f8[:],f8[:],f8[:],f8[:,:],f8[:,:],f8[:],f8[:,:],f8[:,:],f8[:],f8[:,:])',
-    '(n3),(),(),(n3,n3),(n3,n3),(),(n3,n3),(n3,n4),(n2)->(n2,n3)', nopython=True)
+    '(f8[:],f8,f8,f8[:,:],f8[:,:],f8,f8[:,:],f8[:,:],f8[:],f8[:,:])',
+    '(n3),(),(),(n3,n3),(n3,n3),(),(n3,n3),(n3,n4),(n2)->(n2,n3)')
 def oscillAnglesOfHKL_gufunc_(hkl, cc, sc, rMat_c, bMat, wavelength, vMat_s, rMat_e, dummy2, oangs):
-    oscillAnglesOfHKL_(hkl, cc[0], sc[0], rMat_c, bMat, wavelength[0], vMat_s, rMat_e, oangs)
+    oscillAnglesOfHKL_(hkl, cc, sc, rMat_c, bMat, wavelength, vMat_s, rMat_e, oangs)
 
 VEC2 = np.empty((2,))
 vInv_ref = np.array([1.,1.,1.,0.,0.,0.])
@@ -248,7 +258,7 @@ def oscillAnglesOfHKL(hkl, chi, rMat, bMat, wavelength, vInv_s=vInv_ref, beamVec
         vMat_s, rMat_e, VEC2, out)
 
 
-@jit(nopython=True, nogil=True)
+@jit('i8(f8,f8[:])')
 def find_in_range_(value, spans):
     """find the index in spans where value >= spans[i] and value < spans[i].
 
@@ -275,33 +285,9 @@ def find_in_range_(value, spans):
             li = mi+1
     return li
 
+
 twopi = 2 * np.pi
-@jit(nopython=True, nogil=True)
-def map_angle_(angle, offset):
-    """
-    Recasts an angle to the range [offset,offset+2*pi]
-
-    """
-    return np.mod(angle-offset, twopi)+offset
-
-
-@jit('b1(i8,i8,i8,i8,f8[:,:],f8)', nopython=True, nogil=True)
-def check_dilated_(eta, ome, dpix_eta, dpix_ome, etaOmeMap, threshold):
-    """
-    Check if there exists a sample over the given threshold in the etaOmeMap
-    at (eta, ome), with a tolerance of (dpix_eta, dpix_ome) samples.
-    """
-    i_max, j_max = etaOmeMap.shape
-    ome_start, ome_stop = max(ome - dpix_ome, 0), min(ome + dpix_ome + 1, i_max)
-    eta_start, eta_stop = max(eta - dpix_eta, 0), min(eta + dpix_eta + 1, j_max)
-    for i in range(ome_start, ome_stop):
-        for j in range(eta_start, eta_stop):
-            if etaOmeMap[i,j] > threshold:
-                return True
-    return False
-
-#'(f8[:],f8,f8[:,:], f8,f8[:],f8[:],i8, f8,f8[:],f8[:],i8)', 
-@jit(nopython=True, nogil=True)
+@jit('(f8[:],f8,f8[:,:], f8,f8[:],f8[:],i8, f8,f8[:],f8[:],i8)')
 def angle_is_hit_(angles, threshold, etaOmeMap,
                   eta_offset, valid_eta_spans, etaEdges, dpix_eta,
                   ome_offset, valid_ome_spans, omeEdges, dpix_ome):
@@ -318,43 +304,57 @@ def angle_is_hit_(angles, threshold, etaOmeMap,
     the hit percentage.
 
     """
-    if np.isnan(angles[0]):
+    if isnan(angles[0]):
         return (0,0)
-    eta = map_angle_(angles[1], eta_offset)
+
+    eta = ((angles[1] - eta_offset) % twopi) + eta_offset
     if find_in_range_(eta, valid_eta_spans) & 1 == 0:
         # index is even: out of valid eta spans
         return (0,0)
-    ome = map_angle_(angles[2], ome_offset)
+
+    ome = ((angles[2] - ome_offset) % twopi) + ome_offset
     if find_in_range_(ome, valid_ome_spans) & 1 == 0:
         # index is even: out of valid ome spans
         return (0,0)
+
     # discretize the angles
     eta_idx = find_in_range_(eta, etaEdges) - 1
     if eta_idx < 0:
         # out of range
         return (0,0)
+
     ome_idx = find_in_range_(ome, omeEdges) - 1
     if ome_idx < 0:
         # out of range
         return (0,0)
-    out = check_dilated_(eta_idx, ome_idx, dpix_eta, dpix_ome, etaOmeMap, threshold)
-    return (1 if out else 0, 1)
+
+    ome_start = max(ome_idx - dpix_ome, 0)
+    ome_stop  = min(ome_idx + dpix_ome + 1, etaOmeMap.shape[0])
+    eta_start = max(eta_idx - dpix_eta, 0)
+    eta_stop  = min(eta_idx + dpix_eta + 1, etaOmeMap.shape[1])
+    for i in range(ome_start, ome_stop):
+        for j in range(eta_start, eta_stop):
+            if etaOmeMap[i,j] > threshold:
+                return (1, 1)
+                
+    return (0, 1)
 
 
 @guvectorize(
-'(f8[:],i8[:],f8[:],f8[:,:,:], f8[:],f8[:],f8[:],i8[:], f8[:],f8[:],f8[:],i8[:], f8[:],i8[:])',
-'(n3),(),(m1),(m1,m2,m3), (),(m4),(m5),(), (),(m7),(m8),(), (n2)->(n2)', nopython=True)
+'(f8[:],i8,f8[:],f8[:,:,:], f8,f8[:],f8[:],i8, f8,f8[:],f8[:],i8, f8[:],i8[:])',
+'(n3),(),(m1),(m1,m2,m3), (),(m4),(m5),(), (),(m7),(m8),(), (n2)->(n2)')
 def angle_is_hit_gufunc_(ang, hkl, threshold, etaOmeMaps,
         offset_eta, valid_eta_spans, etaEdges, dpix_eta,
         offset_ome, valid_ome_spans, omeEdges, dpix_ome, 
         dummy2, out):
     out[0], out[1] = angle_is_hit_(
-            ang, threshold[hkl[0]], etaOmeMaps[hkl[0]],
-            offset_eta[0], valid_eta_spans, etaEdges, dpix_eta[0],
-            offset_ome[0], valid_ome_spans, omeEdges, dpix_ome[0])
+            ang, threshold[hkl], etaOmeMaps[hkl],
+            offset_eta, valid_eta_spans, etaEdges, dpix_eta,
+            offset_ome, valid_ome_spans, omeEdges, dpix_ome)
 
 
-@jit(nopython=True, nogil=True)
+tiny64 = np.finfo(np.float64).min
+@jit('(f8[:], f8[:,:],f8,f8,f8[:,:],f8,f8[:,:],f8[:,:], i8[:],f8[:],f8[:,:,:], f8,f8[:],f8[:],i8, f8,f8[:],f8[:],i8)')
 def paintGridThis_(quat,
     hkls, cc, sc, bMat, wavelength, vMat_s, rMat_e, 
     hkl_ix, thresholds, etaOmeMaps,
@@ -378,19 +378,18 @@ def paintGridThis_(quat,
     return float(hits) / (float(total) + tiny64)
 
 @guvectorize(
-    '(f8[:], f8[:,:],f8[:],f8[:],f8[:,:],f8[:],f8[:,:],f8[:,:], i8[:],f8[:],f8[:,:,:],  f8[:],f8[:],f8[:],i8[:], f8[:],f8[:],f8[:],i8[:], f8[:])',
-    '(n4),   (mm,n3),(),   (),   (n3,n3),(),   (n3,n3),(n3,n3), (mm), (m1), (m1,m2,m3), (),   (m4), (m5), (),    (),   (m7), (m8), ()  -> ()',
-    nopython=True, target='cpu')
+    '(f8[:], f8[:,:],f8,f8,f8[:,:],f8,f8[:,:],f8[:,:], i8[:],f8[:],f8[:,:,:],  f8,f8[:],f8[:],i8,f8,f8[:],f8[:],i8, f8[:])',
+    '(n4),   (mm,n3),(),(),(n3,n3),(),(n3,n3),(n3,n3), (mm), (m1), (m1,m2,m3), (), (m4), (m5),(),(),(m7), (m8), ()  -> ()')
 def paintGridThis_gufunc_(quat,
     hkls, cc, sc, bMat, wavelength, vMat_s, rMat_e, 
     hkl_ix, threshold, etaOmeMaps,
     offset_eta, valid_eta_spans, etaEdges, dpix_eta,
     offset_ome, valid_ome_spans, omeEdges, dpix_ome, out):
     out[0] = paintGridThis_(quat,
-            hkls, cc[0], sc[0], bMat, wavelength[0], vMat_s, rMat_e,
+            hkls, cc, sc, bMat, wavelength, vMat_s, rMat_e,
             hkl_ix, threshold, etaOmeMaps,
-            offset_eta[0], valid_eta_spans, etaEdges, dpix_eta[0],
-            offset_ome[0], valid_ome_spans, omeEdges, dpix_ome[0])
+            offset_eta, valid_eta_spans, etaEdges, dpix_eta,
+            offset_ome, valid_ome_spans, omeEdges, dpix_ome)
 
 def paintGridThis(quats,
     hkls, chi, bMat, wavelength, vInv_s, beamVec, etaVec,
