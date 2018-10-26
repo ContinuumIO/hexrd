dd03fe6ea90548c9abc6be12c56070ab23157925
Oscar Villellas <oscar.villellas@continuum.io>
2018-10-26 11:57:47 +0200

    added strided argument tests for make_beam_rmat
    also fixed type with atol (1e+10 was meant to be 1e-10)

--- hexrd/transforms/tests/test_make_beam_rmat.py
+++ hexrd/transforms/tests/test_make_beam_rmat.py
@@ -14,6 +14,8 @@ from numpy.testing import assert_allclose
 
 import pytest
 
+ATOL_IDENTITY = 1e-10
+
 all_impls = pytest.mark.parametrize('make_beam_rmat_impl, module_name',
                                     [(numpy_make_beam_rmat, 'numpy'),
                                      (capi_make_beam_rmat, 'capi'),
@@ -21,9 +23,12 @@ all_impls = pytest.mark.parametrize('make_beam_rmat_impl, module_name',
                                      (default_make_beam_rmat, 'default')]
                                 )
 
+# ------------------------------------------------------------------------------
+
+# Test reference frame result
 
 @all_impls
-def test_reference_beam_mat(make_beam_rmat_impl, module_name):
+def test_reference_beam_rmat(make_beam_rmat_impl, module_name):
     """Building from the standard beam_vec and eta_vec should
     yield an identity matrix.
 
@@ -33,8 +38,12 @@ def test_reference_beam_mat(make_beam_rmat_impl, module_name):
 
     rmat = make_beam_rmat_impl(cnst.beam_vec, cnst.eta_vec)
 
-    assert_allclose(rmat, cnst.identity_3x3)
+    assert_allclose(rmat, cnst.identity_3x3, atol=ATOL_IDENTITY)
 
+
+# ------------------------------------------------------------------------------
+
+# Test error conditions
 
 @all_impls
 def test_zero_beam_vec(make_beam_rmat_impl, module_name):
@@ -51,6 +60,10 @@ def test_colinear_beam_eta_vec(make_beam_rmat_impl, module_name):
         make_beam_rmat_impl(cnst.beam_vec, cnst.beam_vec)
 
 
+# ------------------------------------------------------------------------------
+
+# Test orthonormal results
+
 @all_impls
 def test_orthonormal_1(make_beam_rmat_impl, module_name):
     beam_vec = np.array([1.0, 2.0, 3.0])
@@ -66,7 +79,7 @@ def test_orthonormal_1(make_beam_rmat_impl, module_name):
 
     # dot(A, A.T) == Identity seems a good orthonormality check
     # Note: atol needed as rtol is not useful for '0.' entries.
-    assert_allclose(np.dot(rmat, rmat.T), cnst.identity_3x3, atol=1e+10)
+    assert_allclose(np.dot(rmat, rmat.T), cnst.identity_3x3, atol=ATOL_IDENTITY)
 
 
 @all_impls
@@ -82,7 +95,7 @@ def test_orthonormal_2(make_beam_rmat_impl, module_name):
 
     # dot(A, A.T) == Identity seems a good orthonormality check
     # Note: atol needed as rtol is not useful for '0.' entries.
-    assert_allclose(np.dot(rmat, rmat.T), cnst.identity_3x3, atol=1e+10)
+    assert_allclose(np.dot(rmat, rmat.T), cnst.identity_3x3, atol=ATOL_IDENTITY)
 
 
 @all_impls
@@ -95,4 +108,45 @@ def test_orthonormal_3(make_beam_rmat_impl, module_name):
 
     # dot(A, A.T) == Identity seems a good orthonormality check
     # Note: atol needed as rtol is not useful for '0.' entries.
-    assert_allclose(np.dot(rmat, rmat.T), cnst.identity_3x3, atol=1e+10)
+    assert_allclose(np.dot(rmat, rmat.T), cnst.identity_3x3, atol=ATOL_IDENTITY)
+
+
+# ------------------------------------------------------------------------------
+
+# Test strided inputs
+
+@all_impls
+def test_strided_beam(make_beam_rmat_impl, module_name):
+    buff = np.zeros((3,2), order="C")
+    buff[:,:] = 42.0 # fill with some trash value
+    buff[:,0] = cnst.beam_vec # but set a strided vector to the valid value
+
+    rmat = make_beam_rmat_impl(buff[:,0], cnst.eta_vec)
+
+    # This should result in identity (see test_reference_beam_rmat)
+    assert_allclose(rmat, cnst.identity_3x3, atol=ATOL_IDENTITY)
+
+
+@all_impls
+def test_strided_eta(make_beam_rmat_impl, module_name):
+    buff = np.zeros((3,2), order="C")
+    buff[:,:] = 42.0 # fill with some trash value
+    buff[:,0] = cnst.eta_vec # but set a strided vector to the valid value
+
+    rmat = make_beam_rmat_impl(cnst.beam_vec, buff[:,0])
+
+    # This should result in identity (see test_reference_beam_rmat)
+    assert_allclose(rmat, cnst.identity_3x3, atol=ATOL_IDENTITY)
+
+
+@all_impls
+def test_strided_beam_eta(make_beam_rmat_impl, module_name):
+    buff = np.zeros((3,4), order="C")
+    buff[:,:] = 42.0
+    buff[:,0] = cnst.beam_vec
+    buff[:,2] = cnst.eta_vec
+
+    rmat = make_beam_rmat_impl(buff[:,0], buff[:,2])
+    # This should result in identity (see test_reference_beam_rmat)
+    assert_allclose(rmat, cnst.identity_3x3, atol=ATOL_IDENTITY)
+
